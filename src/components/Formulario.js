158a9f31c81248cd7c0df76bcfc4e46c40f05449
jorboare <jordi.b.arevalo@gmail.com>
2021-02-11 17:36:43 +0100

    formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <form className='col-12'>
+            <fieldset className='text-center'>
+                <legend>Busca bebidas por categoría o Ingrediente</legend>
+            </fieldset>
+
+            <div className='row mt-4'>
+                <div className='col-md-4'>
+                    <input
+                        name='nombre'
+                        className='form-control'
+                        type='text'
+                        placeholder='Buscar por Ingrediente'
+                    />
+                </div>
+                <div className='col-md-4'>
+                    <select
+                        className='form-control'
+                        name='categoría'
+                    >
+                        <option value=''>-- Selecciona Categoría --</option>
+                    </select>
+                </div>
+                <div className='col-md-4'>
+                    <input
+                        type='submit'
+                        className='btn btn-block btn-primary'
+                        value='Buscar Bebidas'
+                    />
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+
+//Crear el Context
+export const CategoriasContext = createContext()
+
+//Provider es donde se encuentran las funciones y state
